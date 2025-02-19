0b11d4c4eb68acc27ec7b137dda93876693b1639
Comfii3 <ej.shaheen@gmail.com>
2025-02-19 16:28:24 +1100

    Seventh

--- String/String/Main.cpp
+++ String/String/Main.cpp
@@ -7,16 +7,23 @@ int main()
 
 	String text1("Hello World");
 	String text2(text1);
-	
+
+	std::cout << "Text in Uppercase:\n";
 	text1.ToUpper();
 	text1.WriteToConsole();
 
+	std::cout << "----------------------------------\n";
+	std::cout << "Text in Lowercase:\n";
 	text1.ToLower();
 	text1.WriteToConsole();
 
-	text1.Length();
-	text1.WriteToConsole();
+	std::cout << "----------------------------------\n";
+	std::cout << "Text's Length:\n";
+	std::cout << "Text length is " << text1.Length() << " characters\n";
 
+	std::cout << "----------------------------------\n";
+	std::cout << "Appended Text:\n";
+	std::cout << "Appended text looks like: " << text1.Append(" How Are You?");
 
 	return 0;
 }
